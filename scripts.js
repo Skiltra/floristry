a138fb4a68e76e8c072405d7113fea795fab6b01
Skiltra <skillscomic71@gmail.com>
2024-09-26 17:26:19 +0100

    Everything else

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+function varus() {
+    window.alert("virus detected, call 00304950923095");
+    window.open();
+    window.location.href = "https://www.google.com";
+}
+
+
+// Wishlist Implementation
+
+// Form
+
+// Cookies Implementation
+
+// Log File
