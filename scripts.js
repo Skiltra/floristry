f3c914bef9052228162959f872de10c13d799fe9
Skiltra <skillscomic71@gmail.com>
2024-09-30 10:22:22 +0100

    Form Added

--- scripts.js
+++ scripts.js
@@ -1,16 +1,9 @@
 
-
 // For later implementation notify
 if (window.location.href.includes("debug.html")) {
     window.alert("serverside fetch, contents")
 }
 
-
-function varus() {
-    window.alert("virus detected, call 00304950923095");
-    window.open();
-    window.location.href = "https://www.google.com";
-}
 // Dark Mode Function
 
 // Wishlist Implementation
